036725a724a48d34a8e005736f72691ed101d6b5
Encr1pt0r <presidor16@gmail.com>
2020-10-06 12:59:15 +0100

    git init

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 
 export default function App() {
+  const a = "hello";
+  let b;
+  var c = "meep"
+  b = "noop";
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Computer Science BSc</Text>
+      <ScrollView horizontal={false}>
+        <View style={styles.itemContainer}>
+          <Text style={styles.moduleText}>Mobile Application Development {a}</Text>
+          <Image source={require('./assets/mobile.jpg')} style={styles.img} />
+        </View>
+        <View style={styles.itemContainer}>
+          <Text style={styles.moduleText}>Software Development Practice {b}</Text>
+          <Image source={require('./assets/sdp.jpg')} style={styles.img} />
+        </View>
+        <View style={styles.itemContainer}>
+          <Text style={styles.moduleText}>Programming III {c}</Text>
+          <Image source={require('./assets/programming.jpg')} style={styles.img} />
+        </View>
+        <View style={styles.itemContainer}>
+          <Text style={styles.moduleText}>Individual Project {b} {c}</Text>
+          <Image source={require('./assets/indie.jpg')} style={styles.img} />
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -17,5 +38,28 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 50,
   },
+  itemContainer: {
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 4,
+    padding: 5,
+    marginVertical: 5,
+  },
+  heading: {
+    fontSize: 30,
+    color: 'blue',
+    marginTop: 10,
+  },
+  img: {
+    width: 250,
+    height: 250,
+  },
+  moduleText: {
+    fontSize: 15,
+    color: 'blue',
+    marginTop: 10,
+    marginBottom: 10,
+  }
 });
